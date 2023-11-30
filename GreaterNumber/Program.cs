9fa423b46824cee77674ffcca41f08919dcda50e
kannstas <k.annstas@gmail.com>
2023-11-30 13:07:45 +0300

    add interfaces

--- GreaterNumber/Program.cs
+++ GreaterNumber/Program.cs
@@ -1,39 +1,65 @@
 ﻿
+using System.Buffers;
+
 public class Test
 {
     public static void Main()
+
     {
+      
 
-        //Console.WriteLine("Введите первое число");
-        //int firstNumber = int.Parse(Console.ReadLine());
+         //задание 5 из лабы 7 на создание собственного интерфейса
+        int number = 0;
+        ulong longNumber = 32313121;
+        string s = "Text";
 
-        //Console.WriteLine("Введите второе число");
-        //int secondNumber = int.Parse(Console.ReadLine());
+        Coordinate coordinate = new Coordinate(21.0, 68.0);
 
-        //int greater = Utils.Greater(firstNumber, secondNumber);
-        //Console.WriteLine($"большее значение {greater}");
+        Utils.Display(number);
+        Utils.Display(s);
+        Utils.Display(coordinate);
+
+        // задание 4 из лабы 7 на интерфейсы
+
+        Console.WriteLine(Utils.IsItFormattable(number));
+
+        Console.WriteLine(Utils.IsItFormattable(longNumber));
+        Console.WriteLine(Utils.IsItFormattable(s));
 
 
-        //Console.WriteLine($"до преобразования {firstNumber}, {secondNumber}");
-        //Utils.Swap(ref firstNumber, ref secondNumber);
-        //Console.WriteLine($"после преобразования {firstNumber}, {secondNumber}");
 
 
-        //int factorialResult;
-        //bool ok;
 
-        //Console.WriteLine("Число для которого рассчитать факториал");
-        //int numberForFactorial = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите первое число");
+        int firstNumber = int.Parse(Console.ReadLine());
 
-        //ok = Utils.Factorial(numberForFactorial, out factorialResult);
+        Console.WriteLine("Введите второе число");
+        int secondNumber = int.Parse(Console.ReadLine());
 
-        //if (ok)
+        int greater = Utils.Greater(firstNumber, secondNumber);
+        Console.WriteLine($"большее значение {greater}");
 
-        //    Console.WriteLine($"Factorial({numberForFactorial}) = {factorialResult}");
 
-        //else
+        Console.WriteLine($"до преобразования {firstNumber}, {secondNumber}");
+        Utils.Swap(ref firstNumber, ref secondNumber);
+        Console.WriteLine($"после преобразования {firstNumber}, {secondNumber}");
 
-        //    Console.WriteLine("Cannot compute this factorial");
+
+        int factorialResult;
+        bool ok;
+
+        Console.WriteLine("Число для которого рассчитать факториал");
+        int numberForFactorial = int.Parse(Console.ReadLine());
+
+        ok = Utils.Factorial(numberForFactorial, out factorialResult);
+
+        if (ok)
+
+            Console.WriteLine($"Factorial({numberForFactorial}) = {factorialResult}");
+
+        else
+
+            Console.WriteLine("Cannot compute this factorial");
 
 
 
@@ -45,7 +71,14 @@ public class Test
     }
 }
 
-class Utils {
+
+interface IPrintable
+{
+    void Print();
+}
+
+class Utils
+{
 
     public static void Swap(ref int firstNumber, ref int secondNumber)
     {
@@ -112,7 +145,64 @@ class Utils {
 
     }
 
+    public static bool IsItFormattable (Object x)
+    {
+        if (x is IFormattable)
+            return true;
+
+        else
+            return false;
+        
+    }
+
+    public static void Display (Object item)
+    {
+        IPrintable ip;
+
+        ip = item as IPrintable;
+
+        if (ip!=null)
+        {
+            ip.Print();
+        }
+        else
+        {
+            Console.WriteLine($"Этот объект не поддерживает интерфейс  IPrintable {item}");
+            
+           
+        }
+
+
+    }
 }
+
+class Coordinate : IPrintable
+{
+    private double x;
+    private double y;
+
+    public Coordinate()
+    {
+        x = 0.0;
+        y = 0.0;
+    }
+
+    public Coordinate(double px, double py)
+    {
+        x = px;
+        y = py;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"({x},{y})");
+    }
+}
+
+
+
+
+
 
 
 
