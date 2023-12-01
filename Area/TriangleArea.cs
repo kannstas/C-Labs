470332bfafef34cac65b137b32760525a2029e20
kannstas <k.annstas@gmail.com>
2023-12-01 12:33:19 +0300

    added constructorsa,checked added method initializations sides, added interface compareto, changed the name method input and output to workWithConsole

--- Area/TriangleArea.cs
+++ Area/TriangleArea.cs
@@ -1,71 +1,123 @@
 ﻿
 
-using System;
 
-Triangle triangle = new Triangle();
+WorkWithConsole();
 
 
-Input();
-
-   static void Input()
+void WorkWithConsole()
 {
-    Triangle triangle = new Triangle();
+    Console.WriteLine("Вы хотите сравнить треугольники или посчитать их площадь?");
 
+    string answer = Console.ReadLine();
 
-    Console.WriteLine("Это равностороний треугольник или нет?");
-    String answer = Console.ReadLine();
+    Triangle triangle = AddSidesInTriangle(answer);
 
-    if (answer.Equals("да"))
+    if (answer.Equals("посчитать"))
+        triangle.PrintInformationAboutTriangle();
 
-    {
-        Console.WriteLine("Введите сторону");
-        double oneSide = double.Parse(Console.ReadLine());
-
-        OutputEquilateralTriangle(triangle, oneSide);
-
-    }
-    else if (answer.Equals("нет"))
-
-    {
-        Console.WriteLine("Введите сторону A");
-        double sideA = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Введите сторону B");
-        double sideB = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Введите сторону C");
-        double sideC = double.Parse(Console.ReadLine());
-
-
-        var tuple = (SideA: sideA, SideB: sideB, SideC: sideC);
-        tuple = triangle.InitializationSides(sideA, sideB, sideC);
-        Output(triangle, tuple);
-
-    }
-
-    static void Output(Triangle triangle, (double SideA, double SideB, double SideC) tuple)
-    {
-        Console.WriteLine($"периметр {triangle.CalculateTrianglePerimeter(tuple)}");
-        triangle.CalculateArea(tuple);
-        triangle.PrintSides(tuple);
-    }
-
-    static void OutputEquilateralTriangle(Triangle triangle, double oneSide)
-    {
-        Console.WriteLine($"периметр {triangle.CalculateTrianglePerimeter(triangle.InitializationSides(oneSide))}");
-     
-        triangle.CalculateArea(triangle.InitializationSides(oneSide));
-
-        triangle.PrintSides(oneSide);
-    }
 }
-  
+
+
+
+static Triangle AddSidesInTriangle(string answer)
+{
+
+    if (answer.Equals("посчитать"))
+    {
+        Console.WriteLine("Это равносторонний треуголник или нет?");
+        string newAnswer = Console.ReadLine();
+
+        if (newAnswer.Equals("нет"))
+        {
+            Console.WriteLine("Введите стороны треугольника");
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
+            double sideC = double.Parse(Console.ReadLine());
+
+            return new Triangle(sideA, sideB, sideC);
+        }
+        else if (newAnswer.Equals("да"))
+        {
+
+            Console.WriteLine("Введите стороны треугольника");
+            double oneside = double.Parse(Console.ReadLine());
+
+            return new Triangle(oneside);
+        }
+    }
+
+    if (answer.Equals("сравнить"))
+    {
+        List<Triangle> triangles = new List<Triangle>();
+
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine($"Введите стороны {i + 1}-го треугольника");
+
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
+            double sideC = double.Parse(Console.ReadLine());
+
+            triangles.Add(new Triangle(sideA, sideB, sideC));
+
+        }
+
+        CompareTriangles(triangles);
+
+    }
+
+    return null;
+
+
+}
+
+
+
+
+static void CompareTriangles(List<Triangle> triangles)
+{
+
+
+    IComparable comparable = triangles.First();
+
+    Triangle triangle2 = triangles.Last();
+
+    if (comparable.CompareTo(triangle2) == 0)
+
+        Console.WriteLine($"Треугольники 1 и 2 равны");
+
+    if (comparable.CompareTo(triangle2) == 1)
+
+        Console.WriteLine($"Площадь треугольника 2 больше площади треугольника 1");
+
+    if (comparable.CompareTo(triangle2) == -1)
+
+        Console.WriteLine($"Площадь треугольника 1 больше площади треугольника 2");
+
+
+
+    SortAndInputList(triangles);
+
+}
+
+
+static void SortAndInputList(List<Triangle> triangles)
+{
+    IEnumerable<Triangle> query = triangles.OrderBy(triangles => triangles.Area);
+
+    foreach (Triangle triangle in query)
+    {
+        Console.WriteLine(triangle.Area);
+
+    }
+
+}
 
 
 
 
 
-class Triangle
+class Triangle : IComparable
 {
     private double sideA;
 
@@ -73,99 +125,88 @@ class Triangle
 
     private double sideC;
 
+    public double Area { get; set; } 
+
+    public double Perimeter { get; set; }
 
 
-    public (double sideA, double sideB, double sideC) InitializationSides(double sideA, double sideB, double sideC)
+    public Triangle(double sideA, double sideB, double sideC)
     {
-        sideA = sideA;
-        sideB = sideB;
-        sideC = sideC;
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
 
-        return (sideA, sideB, sideC);
+        this.Perimeter = CalculatePerimeter();
+        this.Area = CalculateArea();
     }
 
-    public double InitializationSides(double oneSide)
+    public Triangle(double sideA)
     {
-        return sideA = oneSide;
+        this.sideA = sideA;
+        this.sideB = sideA;
+        this.sideC = sideA;
+
+        this.Perimeter = CalculatePerimeter();
+        this.Area = CalculateArea();
+
     }
 
-    public double CalculateTrianglePerimeter(double oneSide)
+    int IComparable.CompareTo(object obj)
     {
-        if (ItIsTriangle(oneSide).Equals(true))
+        Triangle triangle1 = (Triangle)obj;
+
+        Triangle triangle2 = this;
+
+        if (triangle1.Area == triangle2.Area) return 0;
+
+        else if (triangle1.Area > triangle2.Area) return 1;
+        else return -1;
+    }
+
+
+
+
+    public double CalculatePerimeter()
+    {
+        if (ItIsTriangle() == true)
         {
-            double perimeter = oneSide * 3;
+            double perimeter = sideA + sideB + sideC;
+
             return perimeter;
         }
         return 0;
     }
 
-    public double CalculateTrianglePerimeter((double sideA, double sideB, double sideC) tuple)
+
+
+    public double CalculateArea()
     {
-        if (ItIsTriangle(tuple).Equals(true))
+        if (ItIsTriangle() == true)
         {
-            double perimeter = tuple.sideA + tuple.sideB + tuple.sideC;
-    
-            return perimeter;
+            double halfPerimeter = Perimeter / 2;
+
+            double areaOfTriangle = Math.Sqrt(halfPerimeter * (halfPerimeter - sideA) * (halfPerimeter - sideB)
+                  * (halfPerimeter - sideC));
+
+            return Math.Round(areaOfTriangle, 2);
+
         }
-        return 0; 
+        return 0;
     }
 
 
-    public void CalculateArea(double oneSide)
-
+    public string PrintSides()
     {
-        if (ItIsTriangle(oneSide).Equals(true))
-        {
-            double areaOfTriangle = Math.Sqrt(CalculateTrianglePerimeter(oneSide) / 2 * Math.Pow(CalculateTrianglePerimeter(oneSide) / 2 - sideA, 3));
-
-            Console.WriteLine($"площадь: {Math.Round(areaOfTriangle, 2)}");
-
-        }
-    }
-
-
-    public void CalculateArea((double sideA, double sideB, double sideC) tuple)
-    {
-        if (ItIsTriangle(tuple).Equals(true))
-        {
-            double halfPerimeter = CalculateTrianglePerimeter(tuple) / 2;
-
-            double areaOfTriangle = Math.Sqrt(halfPerimeter * (halfPerimeter - tuple.sideA) * (halfPerimeter - tuple.sideB)
-                  * (halfPerimeter - tuple.sideC));
-
-            Console.WriteLine($"площадь: {Math.Round(areaOfTriangle, 2)}");
-
-        }
-    }
-
-    public void PrintSides((double sideA, double sideB, double sideC) tuple)
-    {
-        if (ItIsTriangle(tuple).Equals(true))
-        {
-            Console.WriteLine($"Cтороны треугольника {tuple.sideA}, {tuple.sideB}, {tuple.sideC}");
-        }
-    }
-
-
-    public void PrintSides(double oneSide)
-    {
-        if (ItIsTriangle(oneSide).Equals(true))
-        {
-            Console.WriteLine($"Cтороны треугольника равны {oneSide}");
-        }
+        return $"Cтороны треугольника {sideA}, {sideB}, {sideC}";
     }
 
 
 
-    private bool ItIsTriangle((double sideA, double sideB, double sideC) tuple)
+    private bool ItIsTriangle()
     {
 
-
-        if ((tuple.sideA == 0) || (tuple.sideA < 0)
-               || (tuple.sideB == 0) || (tuple.sideB < 0)
-               || (tuple.sideC == 0) || (tuple.sideC < 0))
+        if (sideA <= 0 || sideB <= 0 || sideC <= 0)
         {
-
             Console.WriteLine("Это не треугольник");
             return false;
         }
@@ -175,18 +216,8 @@ class Triangle
         }
     }
 
+    public void PrintInformationAboutTriangle() =>
+        Console.WriteLine($"Периметр треугольника {Perimeter}, площадь треугольника {Area}, {PrintSides()}");
 
-    private bool ItIsTriangle(double oneSide)
-    {
-        if ( oneSide <= 0)
-        {
-            Console.WriteLine("Это не треугольник");
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
 }
 
