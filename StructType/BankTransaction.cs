c6a8565060f04ae102687ee02abe467423ad0a4d
kannstas <k.annstas@gmail.com>
2023-12-04 14:13:23 +0300

    add BankTransaction

---
+++ StructType/BankTransaction.cs
@@ -0,0 +1,27 @@
+﻿
+public class BankTransaction
+{
+    private readonly decimal transactionAmount;
+    private readonly DateTime when;
+
+    public decimal TransactionAmount()
+    {
+        return transactionAmount;
+    }
+
+    public DateTime When()
+    {
+        return when;
+    }
+    public BankTransaction(decimal transactionAmount)
+    {
+        this.transactionAmount = transactionAmount;
+        when = DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+
