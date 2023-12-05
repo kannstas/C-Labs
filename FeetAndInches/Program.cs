179d21dd444714c2884e30abfd892a69731957d1
kannstas <k.annstas@gmail.com>
2023-12-05 13:44:06 +0300

    lab 11 control task. operator overloading

--- FeetAndInches/Program.cs
+++ FeetAndInches/Program.cs
@@ -3,31 +3,108 @@
 Distanse distanse1, distanse2, distance3;
 
 Console.WriteLine("Введите 1 значение футов");
-distanse1.feet = double.Parse(Console.ReadLine());
-
-Console.WriteLine("Введите 2 значение футов");
-distanse2.feet = double.Parse(Console.ReadLine());
+int feet1 = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите 1 значение дюймов");
+double inche1 = double.Parse(Console.ReadLine());
 
-distanse1.inche = double.Parse(Console.ReadLine());
+Distanse firstDistance = new Distanse(feet1, inche1);
+
+
+
+Console.WriteLine("Введите 2 значение футов");
+
+int feet2 = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите 2 значение дюймов");
-distanse2.inche = double.Parse(Console.ReadLine());
+double inche2 = double.Parse(Console.ReadLine());
+
+Distanse secondDistance = new Distanse((int)feet2, inche2);
 
 
-distance3.sumFeet = (int)((distanse1.feet + distanse2.feet) + ((distanse1.inche + distanse2.inche)/12));
-distance3.sumInche = (distanse1.inche + distanse2.inche)%12;
+Distanse sum = firstDistance+secondDistance;
+
+Distanse subtraction = firstDistance - secondDistance;
+
+Console.WriteLine(sum);
 
 
-Console.WriteLine($"Результат: {distance3.sumFeet}'- {distance3.sumInche}''");
+Console.WriteLine(firstDistance == secondDistance);
+
+Console.WriteLine(firstDistance.Equals(secondDistance));
 
 
-public struct Distanse
+
+public class Distanse
 {
-   public double feet;
-   public double inche;
-   public double sumFeet;
-   public double sumInche;
-  
+    private int feet;
+    private double inche;
+
+    private static int sumFeet;
+    private static int sumInche;
+
+
+    private static int subtractionFeet;
+    private static int subtractionInche;
+
+
+    public Distanse(int feet, double inche)
+    {
+        this.feet = feet;
+        this.inche = inche;
+
+    }
+
+
+    public Distanse()
+    {
+
+    }
+
+    public static Distanse operator +(Distanse firstDistance, Distanse secondDistanse)
+    {
+
+        sumFeet = (int)((firstDistance.feet + secondDistanse.feet) + ((firstDistance.inche + secondDistanse.inche) / 12));
+        sumInche = (int)(firstDistance.inche + secondDistanse.inche) % 12;
+        return new Distanse(sumFeet, sumInche);
+
+
+    }
+
+
+    public static Distanse operator -(Distanse firstDistance, Distanse secondDistanse)
+    {
+
+        subtractionFeet = (int)((firstDistance.feet + secondDistanse.feet) - ((firstDistance.inche + secondDistanse.inche) / 12));
+        subtractionInche= (int)(firstDistance.inche - secondDistanse.inche) % 12;
+
+        return new Distanse(subtractionFeet, subtractionInche);
+    }
+
+    public override string ToString()
+    {
+        return $"Результат сложения: {sumFeet}'- {sumInche}''" +
+            $"Результат вычитания: {subtractionFeet}'- {subtractionInche}''";
+
+    }
+
+
+    public static bool operator == (Distanse firstDistanse, Distanse secondDistanse)
+    {
+        if (firstDistanse.feet == secondDistanse.feet && firstDistanse.inche == secondDistanse.inche)
+            return true;
+        else return false;
+    }
+
+    public static bool operator != (Distanse firstDistanse, Distanse secondDistanse)
+    {
+        return !(firstDistanse == secondDistanse);
+    }
+
+    public override bool Equals(Object distanse)
+    {
+        return this == (Distanse)distanse;
+    }
+
+
 }
